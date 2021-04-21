4c4da9f2cd9ddd61c2415c316cdb2eb41c9b5507
Talha&Sami <70809552+Talha-Sami-OS@users.noreply.github.com>
2021-04-21 11:29:45 +0500

    Update ex4_24.c

--- ex4_24.c
+++ ex4_24.c
@@ -1,46 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h> 
 #include <math.h>
-//#include <pthread.h>
-#include <stdlib.h> // For random(), RAND_MAX
-#include <math.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 
-int i         = 0;	
-int amtWanted = 0;
-int totalPts  = 0;
+#define total_points    1000000
+#define threads   1
 
-void *count(void *X)
-{ 
-    /* initialize random numbers */
-    for (i=0; i < amtWanted; i++)
-    {	
-        double X = (double)rand() / RAND_MAX;
-        double Y = (double)rand() / RAND_MAX;
-        
-        if (((X * X) + (Y * Y)) <= 1)
-        {
-            totalPts++;
-        }
-    }
-    return NULL;
-}
-
-int main()
-{
-    printf("\n\nWelcome to Threaded Monte Carlo, by Sean Staz\n\n");
-    srand(time(NULL));
-    pthread_t thread;
-
-    do
-	{
-        printf("Please enter a positive number for the amount of points you would like to generate? \n");
-        scanf("%d", &amtWanted);  	
-	}while (amtWanted <= 0);
-  
-    pthread_create(&thread, NULL, &count, NULL);
-    pthread_join(thread, NULL);
+int value_circle = 0;
+void *runner(void *param); /* the thread in the circle*/
+ 
+int main (int argc, const char * argv[]) {
     
-    double points = 4.0 * totalPts;
-    double pi = points / amtWanted;
-    printf("The approximate value of pi for the desired amount of points (%d) is: %f \n", amtWanted, pi);  
+    int i = 0 ;
+    double pi_value;    
+    int points_per_thread = total_points / threads;
+    
+    pthread_t runners[threads]; /* the thread identifier */
+ 
+
+    for (i = 0; i < threads; i++)
+        /* create the thread */
+        pthread_create(&runners[i], 0, runner, &points_per_thread);
+     
+    for (i = 0; i < threads; i++)
+    /* wait for the thread to exit */
+        pthread_join(runners[i],NULL);
+     
+    /* estimating Pi */
+    pi_value = 4.0 * value_circle / total_points;
+    printf("The approximate value of Pi = %f\n",pi_value);
+     
     return 0;
+}
+ 
+/* The thread will execute in this function */ 
+void *runner(void *param)
+{
+    int total_points;
+    total_points = *((int *)param);
+    int i;
+    int count = 0;
+    double x,y;
+     
+    for (i = 0; i < total_points; i++) {
+         
+        // to obtain a random numbers point
+        double x = (double)rand() / RAND_MAX;   //creates a double random number
+        double y = (double)rand() / RAND_MAX;
+       
+        if ( sqrt(x*x + y*y) <= 1.0 )   //x, y point is point within the circle
+            count++; 
+        else 
+            return NULL;   
+    }
+     
+    value_circle += count;
+         
+    pthread_exit(0);
+}
