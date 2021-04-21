219904e94e8e50ebfbbce569c9189aca38ae5d7d
Talha&Sami <70809552+Talha-Sami-OS@users.noreply.github.com>
2021-04-21 13:19:44 +0500

    Update ex7_17.c

--- ex7_17.c
+++ ex7_17.c
@@ -1,2 +1,72 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> 
+#include <math.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+#define total_points    1000000
+#define threads   1
+#define slaves    100
+
+int value_circle = 0;
+void *runner(void *param); /* the thread in the circle*/
+ 
+pthread_mutex_t mutex;
+
+int main (int argc, const char * argv[]) {
+    
+    int i = 0 ;
+    double pi_value;    
+    int points_per_thread = total_points / threads;
+    
+    pthread_t runners[threads]; /* the thread identifier */
+  
+    pthread_mutex_init(&mutex,NULL);  //mutex becomes initialised and unlocked
+ 
+
+    for (i = 0; i < threads; i++)
+        /* create the thread */
+        pthread_create(&runners[i], 0, runner, &points_per_thread);
+     
+    for (i = 0; i < threads; i++)
+    /* wait for the thread to exit */
+        pthread_join(runners[i],NULL);
+     
+    /* estimating Pi */
+    printf("Number of total points in circle = %d\n, total_points")
+    pi_value = 4.0 * value_circle / total_points;
+    printf("The approximate value of Pi = %f\n", pi_value);
+    printf("Number of Slaves = %d\n", slaves);
+     
+    return 0;
+}
+ 
+/* The thread will execute in this function */ 
+void *runner(void *param)
+{
+    int total_points;
+    total_points = *((int *)param);
+    int i;
+    int count = 0;
+    double x,y;
+     
+    for (i = 0; i < total_points; i++) {
+         
+        // to obtain a random numbers point
+        double x = (double)rand() / RAND_MAX;   //creates a double random number
+        double y = (double)rand() / RAND_MAX;
+       
+        if ( sqrt(x*x + y*y) <= 1.0 )   //x, y point is point within the circle
+            count++; 
+        else 
+            return NULL;   
+    }
+     
+    pthread_mutex_lock(&mutex);
+    value_circle += count;
+    pthread_mutex_unlock(&mutex);
+         
+    pthread_exit(0);
+}
